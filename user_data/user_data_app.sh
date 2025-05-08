650da101b39d7468e1c87fa8b301671ae6d97106
netanelburkis <NETANEL.NISIM.BUKRIS@GMAIL.COM>
2025-05-08 07:32:55 +0300

    Update user_data_app.sh

--- user_data/user_data_app.sh
+++ user_data/user_data_app.sh
@@ -14,4 +14,4 @@ echo \
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 usermod -aG docker ubuntu
-docker -d --name myapp -e DB_NAME=todo -e DB_USER=myuser -e DB_PASSWORD=pass -e DB_HOST=172.31.30.100 -p 5000:5000 netanelbukris/to_do_list
+docker run -d --name myapp -e DB_NAME=todo -e DB_USER=myuser -e DB_PASSWORD=pass -e DB_HOST=172.31.42.36 -p 5000:5000 netanelbukris/to_do_list
